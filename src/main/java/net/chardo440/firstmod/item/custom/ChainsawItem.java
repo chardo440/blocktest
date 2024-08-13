1b4797ba0394abe9397ef2894d2b683a637bb72f
Chardo <chardo440@gmail.com>
2024-08-13 18:29:59 -0400

    Basic Setup

--- src/main/java/net/chardo440/firstmod/item/custom/ChainsawItem.java
+++ src/main/java/net/chardo440/firstmod/item/custom/ChainsawItem.java
@@ -1,20 +1,24 @@
 package net.chardo440.firstmod.item.custom;
 
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
+import java.util.List;
 import java.util.Objects;
 
 public class ChainsawItem extends Item {
-
-    public ChainsawItem(Properties pproperties) {
-        super(pproperties);
+    public ChainsawItem(Properties pProperties) {
+        super(pProperties);
     }
 
     @Override
@@ -25,12 +29,23 @@ public class ChainsawItem extends Item {
             if(level.getBlockState(pContext.getClickedPos()).is(BlockTags.LOGS)) {
                 level.destroyBlock(pContext.getClickedPos(), true, pContext.getPlayer());
 
-               pContext.getItemInHand().hurtAndBreak(1,((ServerLevel) level), ((ServerPlayer) pContext.getPlayer()),
-                       item -> Objects.requireNonNull(pContext.getPlayer()).onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
-
-
+                pContext.getItemInHand().hurtAndBreak(1, ((ServerLevel) level), ((ServerPlayer) pContext.getPlayer()),
+                        item -> Objects.requireNonNull(pContext.getPlayer()).onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
             }
         }
+
         return InteractionResult.CONSUME;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+        if(Screen.hasShiftDown()) {
+            pTooltipComponents.add(Component.translatable("tooltip.mccourse.chainsaw.tooltip.1"));
+            pTooltipComponents.add(Component.translatable("tooltip.mccourse.chainsaw.tooltip.2"));
+        } else {
+            pTooltipComponents.add(Component.translatable("tooltip.mccourse.chainsaw.tooltip.shift"));
+        }
+
+        super.appendHoverText(pStack, pContext, pTooltipComponents, pTooltipFlag);
     }
 }
